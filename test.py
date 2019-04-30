0663bf0db2510e4b21043761e2854d45e09fdcbf
Joshua Nguyen <jnguyen22@LAB-SCI-214-07L.WHITWORTH.EDU>
2019-04-30 10:57:18 -0700

    Added some ok work

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("Hello")
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt # plotting
+import numpy as np # linear algebra
+import os # accessing directory structure
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+nRowsRead = 1000 # specify 'None' if want to read whole file
+# abalone_original.csv has 4177 rows in reality, but we are only loading/previewing the first 1000 rows
+df1 = pd.read_csv('abalone.csv', delimiter=',', nrows = nRowsRead)
+df1.dataframeName = 'abalone.csv'
+nRow, nCol = df1.shape
+print(f'There are {nRow} rows and {nCol} columns')
+print(df1.head(5))
+pd.plotPerColumnDistribution(df1, 10, 5)
